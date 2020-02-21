e7fdc4712e65d3a1417e4350bb004d86cbc874ad
Wirawit Rueopas <aun.wirawit@gmail.com>
2020-02-20 16:44:00 -0800

    Setup css minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
+const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
+
 const moment = require('moment');
 moment.locale('en');
 
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addFilter('dateIso', date => {
@@ -13,5 +15,25 @@ module.exports = function(eleventyConfig) {
  
   eleventyConfig.addFilter('dateReadable', date => {
     return moment(date).format('LL'); // E.g. May 31, 2019
+  });
+
+  eleventyConfig.addTransform("cssmin", function(content, outputPath) {
+    if (outputPath.endsWith(".css")) {
+      return new CleanCSS({}).minify(content).styles;
+    }
+    return content;
+  });
+
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if (outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
   });
 };
